f9459b0342ff07a2e6092e2ed7605e3d5532945c
marcianomatias <marcianomatias1@hotmail.com>
2024-09-29 18:01:21 -0300

    primeiro commit

--- Conjuntos/conjuntos.py
+++ Conjuntos/conjuntos.py
@@ -12,3 +12,27 @@ print(nunber)
 nunbers = list(nunber)
 print(nunbers)
 print(nunbers[2])
+
+#usanndo for
+carros = set(("gol","fusca","fusca","Kombi"))
+for carro in carros:
+    print(carro)
+
+#usanndo enumerate
+for indice, carro in enumerate(carros):
+    print(f"{indice} : {carro}")
+
+# metodos da classe do set
+
+# {}.union métodos union, intersection, difference
+
+conj_a = {1, 2, 3, 4}
+conj_b = {2, 3, 4, 5}
+print("Union")
+print(conj_a.union(conj_b))
+print("Intersection")
+print(conj_a.intersection(conj_b))
+print("Difference")
+print(conj_a.difference(conj_b))
+
+
