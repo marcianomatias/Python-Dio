0c15fea489dd728110d55d87abbd3c48ba1c10da
marcianomatias <marcianomatias1@hotmail.com>
2024-09-29 13:11:12 -0300

    criação da pasta conjunto

---
+++ Conjuntos/conjuntos.py
@@ -0,0 +1,7 @@
+print(set(([1,2,3,1,3,4])))
+print(set("abacaxi"))
+print(set(("palio","gol","celta","palio",)))
+carro = set(("gol","fusca","fusca","Kombi"))
+print(carro)
+linguagem = {"Python","java","Python"}
+print(linguagem)
