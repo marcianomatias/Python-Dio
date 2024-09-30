c046087b0a3e9a26ecb352cfd0e7a7d3349e4e38
marcianomatias <marcianomatias1@hotmail.com>
2024-09-29 23:09:34 -0300

    primeiro commit

--- Conjuntos/conjuntos.py
+++ Conjuntos/conjuntos.py
@@ -26,13 +26,70 @@ for indice, carro in enumerate(carros):
 
 # {}.union métodos union, intersection, difference
 
-conj_a = {1, 2, 3, 4}
-conj_b = {2, 3, 4, 5}
+conj_a = {1, 2, 3, 4, 5}
+conj_b = {2, 3, 4, 5, 6, 1}
+conj_c = {8, 0}
 print("Union")
 print(conj_a.union(conj_b))
 print("Intersection")
 print(conj_a.intersection(conj_b))
 print("Difference")
 print(conj_a.difference(conj_b))
+print(conj_b.difference(conj_a))
+print("Difference symmetric")
+print(conj_a.symmetric_difference(conj_b))
+print("Issubset")
+print(conj_a.issubset(conj_b))
+print(conj_b.issubset(conj_a))
+print("Issuper")
+print(conj_a.issuperset(conj_b))
+print(conj_b.issuperset(conj_a))
+print("isdisjoint")
+print(conj_a.isdisjoint(conj_b))
+print(conj_a.isdisjoint(conj_c))
+print(conj_c.isdisjoint(conj_b))
+print(conj_c.isdisjoint(conj_a))
+print("add")
+sorteio = {1, 23}
+sorteio.add(25)
+print(sorteio)
+sorteio.add(42)
+print(sorteio)
+sorteio.clear()
+print(sorteio)
+sorteio = {1, 23}
+
+sorteio.copy()
+print(sorteio)
+
+print("discart")
+numeros = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}
+numeros.discard(5)
+print(numeros)
+numeros.discard(45)
+print(numeros)
+print("pop")
+
+numeros.pop()
+numeros.pop()
+numeros.pop()
+print(numeros)
+print("remove")
+numeros = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}
+numeros.remove(1)
+print(numeros)
+print("len")
+numeros = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}
+print(len(numeros))
+
+print("In")
+print(1 in numeros)
+print(10 in numeros)
+
+
+
+
+
+
 
 
