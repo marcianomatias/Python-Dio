515f60d408c56109152871c4d0e96e4d5249dae1
marcianomatias <marcianomatias1@hotmail.com>
2024-09-30 22:53:21 -0300

    Trabalhando com objetos date, datetime

---
+++ Trabalhando com objetos date, datetime e time/datetime.py
@@ -0,0 +1,15 @@
+from datetime import date, datetime, time
+print("="*100)
+
+data = date(2024, 9,30)
+print(data)
+print(date.today())
+
+data_hora = datetime(2023, 7, 10, 10, 30, 25)
+print(data_hora)
+print(datetime.today())
+print("="*100)
+print("objeto time")
+hora = time(10, 20, 0)
+print(hora)
+print("="*100)
